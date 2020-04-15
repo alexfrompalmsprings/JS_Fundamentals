81c3283dae8d5fe514d9cdf15736f293d4dee0b5
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-04-15 10:40:10 -0700

    add prependToString to the README.md

--- README.md
+++ README.md
@@ -24,3 +24,23 @@ appendToString("bar", "Foo"); // "barFoo"
 appendToString("", "test"); // "test"
 appendToString("other test", ""); // "other test"
 ```
+### prependToString
+```JavaScript
+
+
+// Write a function called prependToString, which accepts two strings.
+// The function should return a new string with the second string prepended to the first string.
+
+
+function  prependToString (stringOne,stringTwo){
+  return stringTwo + stringOne;
+}
+
+// Examples:
+prependToString('awesome', 'very'); // 'veryawesome'
+
+prependToString('awesome', 'very'); // 'veryawesome'
+prependToString('world', 'hello '); // 'hello world'
+prependToString('nothing', '') // 'nothing'
+
+```
--- fundamental.js
+++ fundamental.js
@@ -1,10 +1,35 @@
+// 001 - Fundamentals: appendToString
+
+// Write a function called appendToString, which accepts two strings.
+// The function should return a new string which consists of the second string appended to the first string.
 
 function appendToString(stringone,stringtwo) {
   return stringone + stringtwo;
 }
 
+// Examples:
 appendToString("Hello", " World!");
 appendToString("Foo", "bar"); // "Foobar"
 appendToString("bar", "Foo"); // "barFoo"
 appendToString("", "test"); // "test"
 appendToString("other test", ""); // "other test"
+
+
+// 002 - Fundamentals: prependToString
+
+// Write a function called prependToString, which accepts two strings.
+
+// The function should return a new string with the second string prepended to the first string.
+
+
+function  prependToString (stringOne,stringTwo){
+  return stringTwo + stringOne;
+}
+
+
+// Examples:
+prependToString('awesome', 'very'); // 'veryawesome'
+
+prependToString('awesome', 'very'); // 'veryawesome'
+prependToString('world', 'hello '); // 'hello world'
+prependToString('nothing', '') // 'nothing'
