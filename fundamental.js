64d4a575234aeee1b20abc69686dd3f483ece66a
alexfrompalmsprings <alexavila.oc@gmail.com>
2020-04-15 11:30:00 -0700

    add removeFromString to the README and the JS file

--- fundamental.js
+++ fundamental.js
@@ -151,3 +151,27 @@ function repeat(str, times){
 repeat('Matt', 3) // 'MattMattMatt'
 repeat('Elie', 2) // 'ElieElie'
 repeat('Michael', 0) // ''
+
+
+// 008 - Fundamentals: removeFromString
+
+// Write a function called removeFromString, which accepts a string, a starting index (number) and a number of characters to remove.
+
+// The function should return a new string with the characters removed.
+
+function removeFromString(string,index,number){
+  let newStr = string.split('');
+
+  newStr.splice(index,number)
+        .join('')
+
+  return newStr;
+  }
+
+// Examples:
+
+removeFromString('Elie', 2, 2) // 'El'
+removeFromString('Elie', 0, 1) // 'lie'
+removeFromString('Rithm School', 0, 6) // 'School'
+removeFromString('Rithm School', 2, 4) // 'RiSchool'
+removeFromString('Rithm School', 6, 400) // 'Rithm '
